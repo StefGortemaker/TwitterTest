aa94ded90f24cf20d0d315851710dcb1244207aa
Liam-Schippers <31887438+Liam-Schippers@users.noreply.github.com>
2018-05-24 17:03:27 +0200

    Versie 1.8.1 Hoop dat dit goed gaat

--- app/src/main/java/com/example/tweeter/model/Dataprovider.java
+++ app/src/main/java/com/example/tweeter/model/Dataprovider.java
@@ -8,8 +8,4 @@ public class Dataprovider {
 
    public static List<Tweet> tweets = new ArrayList<>() ;
 
-   /*private void addTweet(com.example.tweeter.Tweet tweet){
-       tweets.add(tweet);
-   }
-   */
 }

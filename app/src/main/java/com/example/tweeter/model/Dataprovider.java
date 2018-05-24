11407345aaa71fb099fc6db57a4a40f472c814b9
StefGortemaker <stefgortemaker1997@hotmail.com>
2018-05-24 13:46:56 +0200

    Versie 1.5 dont crash

--- app/src/main/java/com/example/tweeter/model/Dataprovider.java
+++ app/src/main/java/com/example/tweeter/model/Dataprovider.java
@@ -1,6 +1,5 @@
 package com.example.tweeter.model;
 
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,8 +7,4 @@ public class Dataprovider {
 
    public static List<Tweet> tweets = new ArrayList<>() ;
 
-   /*private void addTweet(com.example.tweeter.Tweet tweet){
-       tweets.add(tweet);
-   }
-   */
 }
